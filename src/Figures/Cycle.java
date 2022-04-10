d9c58a5808ad6e253b1767a1eb44fe8464b62a99
AndyGLEBASTA <mpp.andrey2012@gmail.com>
2022-04-11 00:36:51 +0300

    inital com

--- src/Figures/Cycle.java
+++ src/Figures/Cycle.java
@@ -7,15 +7,30 @@ public class Cycle extends Figure {
 double radius;
     public Cycle(ArrayList<Point> nPoint) {
         super(nPoint);
-        this.radius
+        this.radius = getRadius();
     }
     public double getRadius(){
         Point one = nPoint.get(0);
         Point two = nPoint.get(1);
 
-        double Math.sqrt((Math.pow((two.getX() - one.getX()), 2) + Math.pow((two.getY() - one.getY()), 2)))
+        return Math.sqrt((Math.pow((two.getX() - one.getX()), 2) + Math.pow((two.getY() - one.getY()), 2)));
     }
 
+    @Override
+    public String toString() {
 
+     return String.format("Мы создали %s с длиной окружности %.2f и площадью %.2f",
+             getClass().toString(), getPerimetr(),getArea());
+    }
+
+    @Override
+    protected double getArea(){
+        return Math.PI * getRadius()* getRadius();
+    }
+
+    @Override
+    protected double getPerimetr() {
+        return Math.PI * 2 * getRadius();
+    }
 
 }
--- src/Figures/Solution.java
+++ src/Figures/Solution.java
@@ -8,16 +8,17 @@ public class Solution {
     public static void main(String[] args) {
 
         Point p1 = new Point (0, 0);
-        Point p2 = new Point (0, 4);
+        Point p2 = new Point (0, 1);
         Point p3 = new Point (4, 0);
         Point p4 = new Point (4, 4);
         Point p5 = new Point (9, 12);
 
-        ArrayList <Point> nPoint = new ArrayList(Arrays.asList(p1, p2, p3));
+        ArrayList <Point> nPoint1 = new ArrayList(Arrays.asList(p1, p2, p3));
+        ArrayList <Point> nPoint2 = new ArrayList(Arrays.asList(p1, p2));
 
 
-        Nangle Triangle = new Nangle(nPoint);
-
+        Nangle Triangle = new Nangle(nPoint1);
+        Cycle cycle = new Cycle(nPoint2);
 
     }
 }
